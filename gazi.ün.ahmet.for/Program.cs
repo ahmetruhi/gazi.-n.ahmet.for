4234c1aa74e9e6c0d4e2b85f788fa4505e7d49ff
Mert <mertcanuslu18@gmail.com>
2019-12-11 11:45:32 +0300

    Add project files.

---
+++ gazi.ün.ahmet.for/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gazi.ün.ahmet.@for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selam");
+            Console.ReadKey();
+        }
+    }
+}
